9fc3d41db9633340600d5c46dbbb0f5c803cd6c0
Olex <olex@energymodelling.club>
2020-01-07 16:46:49 +0100

    Added copyright and license notice

--- sets.py
+++ sets.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 24 23:38:20 2019
+Copyright 2019 Olexandr Balyk
 
-@author: Olex
+This file is part of Timeslib.
+
+Timeslib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Timeslib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Timeslib.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import pandas as pd
